b397b474b0a4014da94e3cbd649070b7eb24acf8
iamraguls <raguls4477@gmail.com>
2025-04-08 00:35:43 +0530

    added cross orgin

--- src/main/java/com/project/BlogApplication/contoller/AuthController.java
+++ src/main/java/com/project/BlogApplication/contoller/AuthController.java
@@ -6,11 +6,9 @@ import com.project.BlogApplication.dto.RegisterRequest;
 import com.project.BlogApplication.service.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping("/api/v1")
 public class AuthController {
